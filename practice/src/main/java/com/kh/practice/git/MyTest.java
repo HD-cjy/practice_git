512f1674f4f7341f8df15da138a4886fa70bbfcb
HD-cjy <hdcjy5142@gmail.com>
2025-05-26 15:38:30 +0900

    힘들어

--- practice/src/main/java/com/kh/practice/git/MyTest.java
+++ practice/src/main/java/com/kh/practice/git/MyTest.java
@@ -3,4 +3,11 @@ package com.kh.practice.git;
 public class MyTest {
 	private String name;
 	private String number;
+	private String jonna;
+	private String hard;
+	private String sip;
+	private String pal;
+	private String jinjja;
+	private String hooo;
+
 }
